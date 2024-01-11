7c8fad400f69e139b8ef59d59ed72f75356bb7f3
lihazkhan1214 <lehazali123@gmail.com>
2024-01-11 15:45:09 +0500

    first commit

---
+++ java.js
@@ -0,0 +1,20 @@
+// Q no1: Write an arrow function called multiply that takes two parameters a and b and returns their product?
+
+// Q no2: Convert the following function expression into an arrow function:
+// function greet(name) {
+//     return "Hello, " + name + "!";
+// }
+// ?
+// arrow return function
+// const greet=(name)=>"helllo" +name;
+
+// const result=greet("imran");
+// console.log(result)
+
+// const text ="hello*12435435834";
+// console.log(text.length)
+const text=`hello\v
+world 
+
+`
+console.log(text)
